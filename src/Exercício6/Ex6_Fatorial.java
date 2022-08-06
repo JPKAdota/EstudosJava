22406bad8b0117ad8f989c51f8abf400343d8585
Kadota <joaokadota@gmail.com>
2022-08-05 22:09:15 -0300

    Exercícios Java

---
+++ src/Exercício6/Ex6_Fatorial.java
@@ -0,0 +1,21 @@
+package Exercício6;
+
+import java.util.Scanner;
+
+public class Ex6_Fatorial {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Fatorial: ");
+        int fatorial = scan.nextInt();
+
+        int multi = 1;
+
+        System.out.println(fatorial + "! = ");
+        for ( int i = fatorial; i >= 1 ;i -- ) {
+            multi = multi * i;
+        }
+
+        System.out.println(multi);
+    }
+}
