038cb3e7a8f0edb5a03bd005ca9542bf6c2779df
Kadota <joaokadota@gmail.com>
2022-08-06 22:35:22 -0300

    Exercícios Java

--- src/Lista/ExemploOrdenacaoList.java
+++ src/Lista/ExemploOrdenacaoList.java
@@ -2,6 +2,33 @@ package Lista;
 
 public class ExemploOrdenacaoList {
     public static void main(String[] args) {
-
+        System.out.println();
     }
+}
+
+
+class Gato{
+    private String nome;
+    private Integer idade;
+    private  String cor;
+
+    public Gato(String nome, Integer idade, String cor) {
+        this.nome = nome;
+        this.idade = idade;
+        this.cor = cor;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public Integer getIdade() {
+        return idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+
 }
